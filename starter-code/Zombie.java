c1e41ffe28fd052a4506e44ebdf7bf6ea09f5d0f
wanaaaa <wanaaaa@gmail.com>
2016-06-29 15:49:50 -0400

    asdf

--- starter-code/Zombie.java
+++ starter-code/Zombie.java
@@ -5,6 +5,13 @@
 //TODO: Make Skeleton a subclass of monster, and implement the aboutMe method.
 //Example output of the aboutMe method: "I am a Zombie with 4 health and do 10 damage!"
 
-public class Zombie{
+public class Zombie extends Monster{
+    public Zombie(int health, int damage) {
+        super(health, damage);
+    }
 
+    @Override
+    public String aboutMe() {
+        return "I am a Zombie with 4 health and do 10 damage!";
+    }
 }
